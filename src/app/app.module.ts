ca5d7896dbe66cc1f35759d25490064cc9d94635
Essia AYARI <ayariessia228@gmail.com>
2023-11-02 19:44:19 +0100

    projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './app/comp1/menu/menu.component';
+import { AboutusComponent } from './app/comp1/aboutus/aboutus.component';
+import { HomeComponent } from './app/comp1/home/home.component';
+import { EventsActsComponent } from './app/comp1/events-acts/events-acts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutusComponent,
+    HomeComponent,
+    EventsActsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/app/comp1/events-acts/events-acts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-acts',
+  templateUrl: './events-acts.component.html',
+  styleUrls: ['./events-acts.component.css']
+})
+export class EventsActsComponent {
+
+}
