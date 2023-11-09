883aca365f81fabd2f9beab65cf2350e3b937e60
Essia AYARI <ayariessia228@gmail.com>
2023-11-09 09:01:02 +0100

    navbar complited

--- src/app/app/comp1/menu/menu.component.ts
+++ src/app/app/comp1/menu/menu.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements AfterViewInit{
+  ngAfterViewInit() {
+    const minim = document.querySelector(".minimenu") as HTMLElement;
+    const navLinks = document.querySelector(".links") as HTMLElement;
 
+    minim.addEventListener('click', () => {
+      navLinks.classList.toggle('mobile-menu');
+    });
+  }
 }
